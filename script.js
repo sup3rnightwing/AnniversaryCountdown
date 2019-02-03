b50b9191c8e8986f2c0f52b4575480f1efb76ca9
Abhishek Rawat <9627825863rohit@gmail.com>
2019-02-03 15:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var comingdate = new Date("Jan 1, 2019 00:00:00");
+var comingdate = new Date("March 1, 2019 00:00:00");
 
 var d = document.getElementById('d');
 var h = document.getElementById('h');

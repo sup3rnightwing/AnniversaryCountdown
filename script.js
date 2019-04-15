169660bd892afaef1c4f10f5f4a71e921a4c92c0
Abhishek Rawat <abhishekrawat5743@gmail.com>
2019-04-15 20:48:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Setting the Launch Date.
-var comingdate = new Date("March 21, 2019 00:00:00");
+var comingdate = new Date("May 1, 2019 00:00:00");
 
 // Accepting the elements.
 var d = document.getElementById('d');
